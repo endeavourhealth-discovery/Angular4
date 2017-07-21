382d53ccda4c32500ea1e58c81f4d83c23e295a7
Stuart Dunning <stuart@bearclawsolutions.co.uk>
2017-07-21 07:50:26 +0100

    Added the logger module back in

---
+++ src/logger/logger.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+import {LoggerService} from "./logger.service";
+
+@NgModule({
+	imports: [
+	],
+    declarations: [],
+	providers: [
+		LoggerService
+	]
+})
+export class LoggerModule {}
