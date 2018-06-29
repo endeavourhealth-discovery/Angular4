af6c066313282fbf4ff08eaba689637b184e5c73
stuart <stuart@bearclawsolutions.co.uk>
2018-06-29 13:00:22 +0100

    Make all entities the same height and truncate text

--- src/entityViewer/entityViewer.ts
+++ src/entityViewer/entityViewer.ts
@@ -38,10 +38,10 @@ import {Router} from "@angular/router";
         </div>
         <div *ngIf="items?.length > 0" class="row">
             <div *ngFor="let item of items | entityFilterPipe:filterText: primary : secondary | orderBy : orderField: reverseOrder: true | paginate: {id: typeDescription, itemsPerPage: pageSize, currentPage: p } " class="col-md-3">
-                <div [ngClass]="displayClass">
-                    <span><b>{{item[primary]}}</b><br><p *ngIf="secondary">{{item[secondary]}}<p></span>
-                    <i *ngIf="allowDelete" (click)="delete(item)" class="fa fa-trash pull-left" aria-hidden="true" style="color:red"></i>
-                    <i (click)="viewItemDetails(item)" class="fa fa-info-circle pull-right" aria-hidden="true" style="color:mediumblue"></i>
+                <div [ngClass]="displayClass" class="h-100">
+                    <span><b>{{item[primary]}}</b><br><p *ngIf="secondary">{{(item[secondary].length > 200) ? (item[secondary] | slice:0:200)+"...":item[secondary]}}<p></span>
+                    <i *ngIf="allowDelete" (click)="delete(item)" class="fa fa-trash pull-left delete-endeavour-button" aria-hidden="true" style="color:red"></i>
+                    <i (click)="viewItemDetails(item)" class="fa fa-info-circle pull-right info-endeavour-button" aria-hidden="true" style="color:mediumblue"></i>
                 </div>
             </div>
         </div>
