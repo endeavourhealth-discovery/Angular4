ab1a18a46294c2a5d3071a6272752e55d1575f08
Richard Collier <ergosoftuk@gmail.com>
2018-09-23 13:38:40 +0100

    Further fixes/consolidation

--- src/layout/topnav.component.ts
+++ src/layout/topnav.component.ts
@@ -60,35 +60,7 @@ import {ApplicationPolicyAttribute} from "../user-manager/models/ApplicationPoli
 					</div>
 				</div>
 			</div>
-		</div>`,
-	styles: [`
-      .title-bar {
-          position: fixed;
-          background: #fbfbfb;
-          z-index:1001;
-          width: 100%;
-      }
-      .title-text {
-          font-size: 28px;
-          line-height: 50px;
-          color: gray;
-          margin-left:10px;
-          vertical-align: middle;
-      }
-
-      .loggedin-text {
-          color: gray;
-          line-height: 50px;
-          vertical-align: middle;
-          margin-right: 10px;
-      }
-
-      .logo-image {
-          height: 50px;
-          width: 50px;
-          margin: 5px;
-      }
-	`]
+		</div>`
 })
 export class TopnavComponent implements OnInit {
 	currentUser:User;
