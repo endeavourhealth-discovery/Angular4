0a6d36c1160cc69e84563b7874b1469903684c74
stuart <stuart@bearclawsolutions.co.uk>
2018-10-03 10:35:53 +0100

    Change the please wait page to a loading icon

--- src/layout/please-wait.component.ts
+++ src/layout/please-wait.component.ts
@@ -8,14 +8,7 @@ import { Component } from '@angular/core';
 				<div class="module-body">
 					<div class="row">
 						<div class="col-md-12">
-
-							<div class="card">
-								<h3 class="card-header"><i class="fa fa-ban text-danger"></i> Please wait!</h3>
-								<div class="card-block">
-									<h4 class="card-title">Loading configuration</h4>
-									<p class="card-text">You will be taken to the app shortly.</p>
-								</div>
-							</div>
+                            <loadingIndicator></loadingIndicator>							
 						</div>
 					</div>
 				</div>
